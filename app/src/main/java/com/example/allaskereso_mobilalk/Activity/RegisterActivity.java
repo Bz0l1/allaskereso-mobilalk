bfe775c351081b8f909031bc9a4c5a37f0d0801c
Bz0l1 <90034118+Bz0l1@users.noreply.github.com>
2024-05-07 22:15:20 +0200

    First Commit

---
+++ app/src/main/java/com/example/allaskereso_mobilalk/Activity/RegisterActivity.java
@@ -0,0 +1,34 @@
+package com.example.allaskereso_mobilalk.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import com.example.allaskereso_mobilalk.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView login = findViewById(R.id.login);
+        login.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
